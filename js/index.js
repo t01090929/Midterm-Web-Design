39b3395cc681d215f1ca3f95d253372715ec68ba
s60912frank <s60912frank@gmail.com>
2016-04-18 15:02:02 +0800

    add some change that forget to commit

--- js/index.js
+++ js/index.js
@@ -1,28 +1,22 @@
 var main = function () {
+  var foundWithO = $('#found_o');
+  var lostWithO = $('#lost_o');
   $('#foundButton').hover(
     function() {
-      $('#found_no').stop();
-      $('#found_o').stop();
-      $('#found_no').fadeToggle();
-      $('#found_o').fadeToggle();
+      foundWithO.stop();
+      foundWithO.fadeIn();
     },function () {
-      $('#found_no').stop();
-      $('#found_o').stop();
-      $('#found_no').fadeToggle();
-      $('#found_o').fadeToggle();
+      foundWithO.stop();
+      foundWithO.fadeOut();
   });
 
   $('#lostButton').hover(
     function() {
-      $('#lost_o').stop();
-      $('#lost_no').stop();
-      $('#lost_o').fadeToggle();
-      $('#lost_no').fadeToggle();
+      lostWithO.stop();
+      lostWithO.fadeOut();
     },function() {
-      $('#lost_o').stop();
-      $('#lost_no').stop();
-      $('#lost_o').fadeToggle();
-      $('#lost_no').fadeToggle();
+      lostWithO.stop();
+      lostWithO.fadeIn();
   });
 }
 
--- js/item.js
+++ js/item.js
@@ -2,16 +2,23 @@ var myFirebaseRef = new Firebase("https://s60912frank.firebaseio.com/");
 $(document).ready(
   function(){
     var vars = getUrlVars();
+
     myFirebaseRef.child("lostFound/" + vars["type"] + "/" + vars["id"]).on("value",
       function(snapshot){
-        $('.imgContainer a').attr("href",snapshot.val().imageURL);
-        $('.imgContainer img').attr("src",snapshot.val().imageURL);
-        $('#title').text(snapshot.val().title);
-        $('#location').text(snapshot.val().location);
-        $('#time').text(snapshot.val().time);
-        $('#who').text(snapshot.val().who);
-        $('#describe').text(snapshot.val().describe);
-        $(".loading").fadeOut(300);
+        try{
+          $('.imgContainer a').attr("href",snapshot.val().imageURL);
+          $('.imgContainer img').attr("src",snapshot.val().imageURL);
+          $('#title').text(snapshot.val().title);
+          $('#location').text(snapshot.val().location);
+          $('#time').text(snapshot.val().time);
+          $('#who').text(snapshot.val().who);
+          $('#describe').text(snapshot.val().describe);
+          loadingComplete(vars["type"]);
+        }
+        catch(e) {
+          loadingComplete(vars["type"]);
+          alert("查無資料!");
+        }
     });
 
     $("#sideBar").css(
@@ -57,4 +64,17 @@ function getUrlVars()
       vars[hash[0]] = hash[1];
   }
   return vars;
+}
+
+var loadingComplete = function(type){
+  if(type == "lost"){
+    $("#who_f_or_l").text("遺失人");
+  }
+  else if(type == "found"){
+    $("#who_f_or_l").text("拾獲人");
+  }
+  else {
+    $("#who_f_or_l").text("錯誤!!!!!");
+  }
+  $(".loading").fadeOut(300);
 }
