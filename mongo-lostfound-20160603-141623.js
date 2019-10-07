13d0bf2ebfbcafa83962c718b6f60df8b7d8e985
Gmin <s60912frank@gmail.com>
2019-10-07 10:55:49 +0800

    Fix old data import

--- mongo-lostfound-20160603-141623.js
+++ mongo-lostfound-20160603-141623.js
@@ -3,14 +3,14 @@ var connection_string = 'db:27017/YOUR_APP_NAME'; // bad
 mongoClient.connect('mongodb://'+connection_string, function(err, db) {
   if(err) throw err;
   /** data indexes **/
-db.getCollection("data").ensureIndex({
-  "_id": NumberInt(1)
+db.collection("data").ensureIndex({
+  "_id": 1
 },[
   
 ]);
 
 /** data records **/
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "found",
   "title": "3333",
   "location": "33333333",
@@ -18,10 +18,9 @@ db.getCollection("data").insert({
   "time": "2016/04/18 23:56",
   "describe": "55555",
   "imageURL": "http://i.imgur.com/voXKPG2.png",
-  "deleteHash": "m3Z3bnKy25h4POV",
-  "_id": ObjectId("57150350320e4edef6551969")
+  "deleteHash": "m3Z3bnKy25h4POV"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "lost",
   "title": "5555",
   "location": "5555",
@@ -29,10 +28,9 @@ db.getCollection("data").insert({
   "time": "2016/04/19 00:00",
   "describe": "555",
   "imageURL": "http://i.imgur.com/uzyv8RJ.jpg",
-  "deleteHash": "ih3DuvZjROcOyDb",
-  "_id": ObjectId("57150442320e4edef655196a")
+  "deleteHash": "ih3DuvZjROcOyDb"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "found",
   "title": "決鬥LA",
   "location": "home",
@@ -40,10 +38,9 @@ db.getCollection("data").insert({
   "time": "2016/04/19 00:01",
   "describe": "666666666666666666666666",
   "imageURL": "http://i.imgur.com/nYozcqz.jpg",
-  "deleteHash": "scm9UFS5CFkLfc0",
-  "_id": ObjectId("57150482320e4edef655196b")
+  "deleteHash": "scm9UFS5CFkLfc0"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "lost",
   "title": "手機測試",
   "location": "home",
@@ -51,10 +48,9 @@ db.getCollection("data").insert({
   "time": "2016/04/19 00:04",
   "describe": "測試哦哦哦哦哦哦哦哦哦",
   "imageURL": "http://i.imgur.com/ICMw1tD.jpg",
-  "deleteHash": "DoIgy5OJOP3HHbW",
-  "_id": ObjectId("57150574761fd6e0e20735ca")
+  "deleteHash": "DoIgy5OJOP3HHbW"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "found",
   "title": "誰的男朋友掉了？？",
   "location": "四教",
@@ -62,10 +58,9 @@ db.getCollection("data").insert({
   "time": "2016/04/19 00:07",
   "describe": "電機暖男\n微宅微靦腆\n平常交通工具：火雞",
   "imageURL": "http://i.imgur.com/12A9JaE.jpg",
-  "deleteHash": "nGQz4D6NL3bd3dh",
-  "_id": ObjectId("57150651761fd6e0e20735cb")
+  "deleteHash": "nGQz4D6NL3bd3dh"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "found",
   "title": "宵夜",
   "location": "捷運善導寺站",
@@ -73,10 +68,9 @@ db.getCollection("data").insert({
   "time": "2016/04/19 00:12",
   "describe": "半夜發宵夜文\nㄙㄨㄤˇ辣\nWheeeeeeeeeeeeeeee",
   "imageURL": "http://i.imgur.com/ZoQ40q5.jpg",
-  "deleteHash": "32dl7bFUNFER9uO",
-  "_id": ObjectId("5715076d761fd6e0e20735cc")
+  "deleteHash": "32dl7bFUNFER9uO"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "lost",
   "title": "驚",
   "location": "!!!",
@@ -84,10 +78,9 @@ db.getCollection("data").insert({
   "time": "2016/04/19 10:55",
   "describe": "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
   "imageURL": "http://i.imgur.com/9kqojRS.png",
-  "deleteHash": "LqIFtJscZKeJIID",
-  "_id": ObjectId("57159df6761fd6e0e20735cd")
+  "deleteHash": "LqIFtJscZKeJIID"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "lost",
   "title": "test redirect",
   "location": "gmin",
@@ -95,10 +88,9 @@ db.getCollection("data").insert({
   "time": "2016/04/19 11:00",
   "describe": "66666666666666666666666666666666666666666666666666666666666666666666666666666666",
   "imageURL": "http://i.imgur.com/yHWeY5X.jpg",
-  "deleteHash": "X3a2PTWd35n7o1q",
-  "_id": ObjectId("57159f14761fd6e0e20735ce")
+  "deleteHash": "X3a2PTWd35n7o1q"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "found",
   "title": "testtesttest",
   "location": "666666666666666",
@@ -106,10 +98,9 @@ db.getCollection("data").insert({
   "time": "2016/04/19 13:05",
   "describe": "666666666666666666666666666666666666666",
   "imageURL": "http://i.imgur.com/SW2u7DT.jpg",
-  "deleteHash": "zviI7fbnonrvTsL",
-  "_id": ObjectId("5715bc83a09d603511a42b8f")
+  "deleteHash": "zviI7fbnonrvTsL"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "found",
   "title": "宵夜",
   "location": "~",
@@ -117,10 +108,9 @@ db.getCollection("data").insert({
   "time": "2016/04/19 20:48",
   "describe": "",
   "imageURL": "http://i.imgur.com/XrqKqEc.jpg",
-  "deleteHash": "sP2dMjeOuE4ZFv4",
-  "_id": ObjectId("571629374b6fed8a60006c9a")
+  "deleteHash": "sP2dMjeOuE4ZFv4"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "found",
   "title": "Z5",
   "location": "Taipei",
@@ -128,10 +118,9 @@ db.getCollection("data").insert({
   "time": "2016/04/19 21:48",
   "describe": "",
   "imageURL": "http://i.imgur.com/JMzp2Dz.jpg",
-  "deleteHash": "pbVmCXWFtEpPJOk",
-  "_id": ObjectId("57163728ea3bba65fa414b9f")
+  "deleteHash": "pbVmCXWFtEpPJOk"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "lost",
   "title": "黑色保溫杯",
   "location": "中正館",
@@ -139,10 +128,9 @@ db.getCollection("data").insert({
   "time": "2016/04/19 23:30",
   "describe": "掉了嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚",
   "imageURL": "http://i.imgur.com/9pGMxG9.jpg",
-  "deleteHash": "dDgkwxJzp298N1f",
-  "_id": ObjectId("57164f21ba300d11386f3686")
+  "deleteHash": "dDgkwxJzp298N1f"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "lost",
   "title": "666666666666666",
   "location": "6666666666666666666666",
@@ -150,10 +138,9 @@ db.getCollection("data").insert({
   "time": "2016/04/20 15:30",
   "describe": "66666666666666",
   "imageURL": "",
-  "deleteHash": "",
-  "_id": ObjectId("5717302659fe489b26f0bfed")
+  "deleteHash": ""
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "lost",
   "title": "女朋友",
   "location": "心",
@@ -161,10 +148,9 @@ db.getCollection("data").insert({
   "time": "2016/04/20 15:30",
   "describe": "我的女友呢",
   "imageURL": "",
-  "deleteHash": "",
-  "_id": ObjectId("5717302c59fe489b26f0bfee")
+  "deleteHash": ""
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "lost",
   "title": "66666666666",
   "location": "66666666",
@@ -172,10 +158,9 @@ db.getCollection("data").insert({
   "time": "2016/04/20 15:31",
   "describe": "6666666",
   "imageURL": "http://i.imgur.com/PpF8yHd.png",
-  "deleteHash": "qbmXbOopwTZqE3x",
-  "_id": ObjectId("5717305359fe489b26f0bfef")
+  "deleteHash": "qbmXbOopwTZqE3x"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "lost",
   "title": "Did anyone see my boyfriend?",
   "location": "My heart",
@@ -183,10 +168,9 @@ db.getCollection("data").insert({
   "time": "2016/04/20 15:36",
   "describe": "He is God.",
   "imageURL": "http://i.imgur.com/iWlHpxa.jpg",
-  "deleteHash": "TnQQ14FA7SmX76G",
-  "_id": ObjectId("5717318959fe489b26f0bff0")
+  "deleteHash": "TnQQ14FA7SmX76G"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "found",
   "title": "000000000000",
   "location": "0000000",
@@ -194,10 +178,9 @@ db.getCollection("data").insert({
   "time": "2016/04/20 15:43",
   "describe": "000000000",
   "imageURL": "",
-  "deleteHash": "",
-  "_id": ObjectId("5717331c59fe489b26f0bff1")
+  "deleteHash": ""
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "found",
   "title": "666666",
   "location": "666666666",
@@ -205,21 +188,9 @@ db.getCollection("data").insert({
   "time": "2016/04/20 16:01",
   "describe": "666666666666",
   "imageURL": "http://i.imgur.com/fw49NEy.png",
-  "deleteHash": "hozb68Uh1txvbHC",
-  "_id": ObjectId("5717374661f8ae8c0a22288c")
+  "deleteHash": "hozb68Uh1txvbHC"
 });
-db.getCollection("data").insert({
-  "type": "found",
-  "title": "0000000000000",
-  "location": "000000000",
-  "who": "0000000000",
-  "time": "2016/04/25 13:18",
-  "describe": "0000000000000",
-  "imageURL": "http://i.imgur.com/sQlrNBT.jpg",
-  "deleteHash": "Ct3hquUPLuouRc7",
-  "_id": ObjectId("571db6a8015a95258111d6f2")
-});
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "found",
   "title": "9999999999",
   "location": "99999999",
@@ -227,10 +198,9 @@ db.getCollection("data").insert({
   "time": "2016/04/28 14:30",
   "describe": "999999999999999999",
   "imageURL": "http://i.imgur.com/ByYk1zS.jpg",
-  "deleteHash": "9VfHL8dpQTzriVm",
-  "_id": ObjectId("5721ae05015a95258111d6f3")
+  "deleteHash": "9VfHL8dpQTzriVm"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "lost",
   "title": "Hi is this still alive?",
   "location": "I think so",
@@ -238,10 +208,9 @@ db.getCollection("data").insert({
   "time": "2016/05/01 00:23",
   "describe": "",
   "imageURL": "http://i.imgur.com/cH4psQU.jpg",
-  "deleteHash": "sWS3Q7aQUjRIx7g",
-  "_id": ObjectId("5724dbee2eb2238a826b8f1a")
+  "deleteHash": "sWS3Q7aQUjRIx7g"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "lost",
   "title": "自己一個人!",
   "location": "家",
@@ -249,10 +218,9 @@ db.getCollection("data").insert({
   "time": "2016/05/12 23:38",
   "describe": "嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚嗚邊緣",
   "imageURL": "http://i.imgur.com/1lykRGq.jpg",
-  "deleteHash": "gS0Zdy585AxjNRw",
-  "_id": ObjectId("5734a2f8044c451b2b9ff4d4")
+  "deleteHash": "gS0Zdy585AxjNRw"
 });
-db.getCollection("data").insert({
+db.collection("data").insertOne({
   "type": "lost",
   "title": "React Example 1 - A simple timer.",
   "location": "0",
@@ -260,8 +228,7 @@ db.getCollection("data").insert({
   "time": "2016/05/14 11:52",
   "describe": "",
   "imageURL": "",
-  "deleteHash": "",
-  "_id": ObjectId("5736a0aa044c451b2b9ff4d5")
+  "deleteHash": ""
 });
 
 })
